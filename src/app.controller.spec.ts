8de79ec4b1ad00357ca2ecd60d1929f88371f639
hainguyen <hainguyen8086@gmail.com>
2026-06-02 13:53:33 +0700

    test variable

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,8 +4,10 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  const oldDemoMessage = process.env.RAILWAY_DEMO_MESSAGE;
 
   beforeEach(async () => {
+    process.env.RAILWAY_DEMO_MESSAGE = 'demo-from-test';
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
@@ -14,9 +16,17 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
+  afterEach(() => {
+    if (oldDemoMessage === undefined) {
+      delete process.env.RAILWAY_DEMO_MESSAGE;
+      return;
+    }
+    process.env.RAILWAY_DEMO_MESSAGE = oldDemoMessage;
+  });
+
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World Redeploy3!');
+    it('should return hello world and railway variable', () => {
+      expect(appController.getHello()).toBe('Hello World!\ndemo-from-test');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Redeploy2!';
+    const railwayDemoVar = process.env.RAILWAY_DEMO_MESSAGE ?? 'not-set';
+    return `Hello World!\n${railwayDemoVar}`;
   }
 }
